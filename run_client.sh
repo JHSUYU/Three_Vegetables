2406318d317f22f47b82b4735b29cb33e4f15c60
181830014 <181830014@smail.nju.edu.cn>
2022-10-21 17:39:58 +0000

    Changes to be committed: 	new file:   observer.log.20221017 	new file:   run_client.sh 	new file:   run_server.sh

---
+++ run_client.sh
@@ -0,0 +1,9 @@
+./build/bin/obclient
+###
+ # @Author: 181830014 181830014@smail.nju.edu.cn
+ # @Date: 2022-10-17 23:04:19
+ # @LastEditors: 181830014 181830014@smail.nju.edu.cn
+ # @LastEditTime: 2022-10-17 23:04:30
+ # @FilePath: /source/Three_Vegetables/run_client.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
---
+++ run_server.sh
@@ -0,0 +1,9 @@
+###
+ # @Author: 181830014 181830014@smail.nju.edu.cn
+ # @Date: 2022-10-17 22:34:51
+ # @LastEditors: 181830014 181830014@smail.nju.edu.cn
+ # @LastEditTime: 2022-10-17 22:35:51
+ # @FilePath: /source/Three_Vegetables/run_server.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
+./build/bin/observer -f ./etc/observer.ini
